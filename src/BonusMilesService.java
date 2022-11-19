1e463ad23e3df924c8f68d76090373edd4ce4959
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2022-11-19 11:39:09 +0300

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+
+    public long calculate (int cost) {
+
+        int prise = 10_000;
+        int bonusMiles = 10;
+
+        int miles = prise / bonusMiles;
+
+        return miles;
+
+    }
+}
