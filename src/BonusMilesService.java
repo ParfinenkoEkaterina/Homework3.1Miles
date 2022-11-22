89a639c1240f1c6a78c05e121f2fbe31ef8128bd
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2022-11-22 19:54:25 +0300

    BonusMilesService optimization

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,13 +1,7 @@
 public class BonusMilesService {
 
-    public long calculate (int cost) {
-
-        int prise = 10_000;
-        int bonusMiles = 10;
-
-        int miles = prise / bonusMiles;
-
-        return miles;
+    public long calculate(int cost) {
+        return cost / 20;
 
     }
 }
